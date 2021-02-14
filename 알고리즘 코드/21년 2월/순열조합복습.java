3d0d0af1db0144fc371eeffec7687abf1e39fcab
Jeus <40746446+jeus95@users.noreply.github.com>
2021-02-14 23:19:18 +0900

    Add files via upload

---
+++ 알고리즘 코드/21년 2월/순열조합복습.java
@@ -0,0 +1,99 @@
+package com.study16;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 순열조합복습 {
+		static int N;
+		static int[] numbers;
+		static boolean[] isSelected;
+		static int totalCnt;
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			Scanner sc = new Scanner(System.in);
+			N = sc.nextInt();
+			
+			numbers = new int[N];
+			
+			isSelected = new boolean[7];
+			
+			
+			int M = sc.nextInt();
+			totalCnt = 0;
+			
+			
+			switch (M) {
+			case 1:
+				dice1(0);
+				break;
+			case 2:
+				dice2(0);
+				break;
+			case 3:
+				dice3(0,1);
+				break;
+			case 4:
+				dice4(0,1);
+			}
+			System.out.println("총 경우의 수 : " + totalCnt);
+			
+			
+		}
+		// 중복 순열: nㅠr ==> n^r
+		private static void dice1(int cnt) {
+			if(cnt == N)
+			{
+				totalCnt++;
+				System.out.println(Arrays.toString(numbers));
+				return;
+			}
+			for(int i =1; i<=6;i++) {
+				numbers[cnt]=i;
+				dice1(cnt+1);
+			}
+		}
+		//순열 : nPr ==> n! 6P3 => 6*5*4
+		private static void dice2(int cnt) {
+			if(cnt == N)
+			{
+				totalCnt++;
+				System.out.println(Arrays.toString(numbers));
+				return;
+			}
+			for(int i =1; i<=6;i++) {
+				numbers[cnt]=i;
+				isSelected[i]=true;
+				dice2(cnt+1);
+				isSelected[i]=false;
+			}
+		}//중복 조합 nHr : 6H3 : n+r-1Cr : 8C3 -> 56
+		private static void dice3(int cnt, int start) {
+			if(cnt==N) {
+				totalCnt++;
+				System.out.println(Arrays.toString(numbers));
+				return;
+			}
+			
+			
+			for(int i = start; i<=6;i++) {
+				numbers[cnt]=i;
+				dice3(cnt+1,i);
+			}
+		}
+			//조합 nCr : 6C3 : 20
+			private static void dice4(int cnt, int start) {
+				if(cnt==N) {
+					totalCnt++;
+					System.out.println(Arrays.toString(numbers));
+					return;
+				}
+				
+				
+				for(int i = start; i<=6;i++) {
+					numbers[cnt]=i;
+					dice4(cnt+1,i+1);
+				}
+	}
+	}
+
